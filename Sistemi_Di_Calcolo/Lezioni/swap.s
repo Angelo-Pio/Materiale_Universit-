91a1a7be2f030cda447b45f5a4b3006ab2a978f8
Angelo-Pio <angelopompeo0@gmail.com>
2022-03-15 15:34:52 +0100

    L

---
+++ Sistemi_Di_Calcolo/Lezioni/swap.s
@@ -0,0 +1,11 @@
+.globl swap
+swap:                   # void swap(char* x, char* y){
+
+    movl 4(%esp),%ecx   #     char* c = x;
+    movl 8(%esp),%edx   #     char* d = y;
+    movb (%ecx),%al     #     char al = *c;
+    movb (%edx), %ah    #     char ah = *d,
+    movb %ah,(%ecx)     #     *c = ah ;
+    movb %al,(%edx)     #     *d = al;
+
+    
